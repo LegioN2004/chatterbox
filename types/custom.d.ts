9b56fdfc7c65878a8c8b3f4dca6196a1a256dc0b
legion2004 <milindabarua04@gmail.com>
2025-11-16 19:01:31 +0530

    added declarations to not throw css errors

---
+++ types/custom.d.ts
@@ -0,0 +1,10 @@
+// Module declarations for side-effect CSS imports used by third-party packages
+declare module '*.css'
+declare module '*.module.css'
+
+// Specific package CSS imports used in the app
+declare module '@stream-io/video-react-sdk/dist/css/styles.css'
+declare module 'stream-chat-react/dist/css/v2/index.css'
+
+export { };
+
